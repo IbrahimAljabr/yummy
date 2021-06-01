3ecc7a44df6f6b60030e0e8a07ae17e0804f84c6
Raghad <raghadmustafa96@yahoo.com>
2021-06-01 18:12:15 +0300

    add reciepe

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Recipes from './src/component/find.js';
+import Recipes from './src/component/recipe.js';
 // import { Provider } from 'react-redux';
 // import store from './src/store/index.js';
 
